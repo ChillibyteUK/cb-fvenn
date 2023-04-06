36a2be9b4c7b806056788c730d49025cc0665a3f
ChillibyteUK <dan@chillibyte.co.uk>
2023-04-06 15:05:16 +0100

    undiddled

--- page-templates/blocks/cb_front_hero.php
+++ page-templates/blocks/cb_front_hero.php
@@ -5,7 +5,6 @@
             <div class="col-lg-6 hero__content d-flex flex-column justify-content-center order-2 order-lg-1 py-5" data-aos="fade">
                 <h1 class="mb-4">fVenn</h1>
                 <h2 class="hero__content">The specialist marketing consultancy for the asset management industry</h2>
-                more diddling
             </div>
             <div class="col-lg-6 hero__image order-1 order-lg-2 d-flex justify-content-center align-items-center py-5">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/fvenn-logo--wo.svg" alt="" width="354" height="320">
