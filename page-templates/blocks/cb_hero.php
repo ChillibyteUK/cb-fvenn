763f02f0e3bfd03db8d8c8882c8381f6865a1c44
ChillibyteUK <dan@chillibyte.co.uk>
2023-04-18 10:06:11 +0100

    confirmed github pull

--- page-templates/blocks/cb_hero.php
+++ page-templates/blocks/cb_hero.php
@@ -4,7 +4,6 @@
         <div class="row g-4 h-100 mx-auto">
             <div class="col-lg-6 hero__content d-flex align-items-end order-2 order-lg-1 py-5" data-aos="fade">
                 <h1><?=get_field('title')?></h1>
-                TEST COMMIT
             </div>
             <div class="col-lg-6 hero__image order-1 order-lg-2 d-flex justify-content-center align-items-center py-5">
                 <img src="<?=get_stylesheet_directory_uri()?>/img/fvenn-logo--nostrap.svg" alt="" width="320" height="305">
