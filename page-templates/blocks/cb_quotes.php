4f8aa843cb3f64d914fa46e22e0c1eb8431dd6a1
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-05-10 16:58:19 +0100

    wide quote if first

--- page-templates/blocks/cb_quotes.php
+++ page-templates/blocks/cb_quotes.php
@@ -6,8 +6,18 @@
                 'post_type' => 'testimonials',
                 'posts_per_page' => -1
             ));
+            $c = 0;
             while ($t->have_posts()) {
                 $t->the_post();
+                if ($c == 0) {
+                    ?>
+                <div class="col-12 quote">
+                    <div class="quote__body"><?=get_the_content()?></div>
+                    <div class="quote__attr"><strong><?=get_the_title()?></strong> - <?=get_field('company_name',get_the_ID())?></div>
+                </div>
+                    <?php
+                    $c++;
+                }
                 ?>
                 <div class="col-md-6 quote">
                     <div class="quote__body"><?=get_the_content()?></div>
